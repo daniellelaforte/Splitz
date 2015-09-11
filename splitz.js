033b0e2b01c19b7f897983405ea2e71c2d318d37
Danielle Laforte <daniellelaforte@Danielles-MacBook-Pro-2.local>
2015-09-10 23:11:08 -0600

    bugs

--- splitz.js
+++ splitz.js
@@ -2,16 +2,28 @@ angular.module("bear", []);
 
 angular.module("bear").controller("polarbearcontroller", ["$scope", function($scope){
 	$scope.message = "Add another price";
-	$scope.classname="forthehiddenform";
+	//$scope.classname="forthehiddenform";
+	$scope.price4="";
+	$scope.showthis=false;
+	$scope.counter=0;
+	$scope.price4="";
+	$scope.showthis1=false;
+
 	
 	$scope.Submit = function() {
 			//event.preventDefault(); //don't need this - angular does this automatically
 		   // $scope.firstname=$scope.price1;
-		      $scope.total=Number($scope.price1) + Number($scope.price2) + Number($scope.price3) + Number($scope.tax);
+		      $scope.total=Number($scope.price1) + Number($scope.price2) + Number($scope.price3) + Number($scope.price4) + Number($scope.price5) + Number($scope.tax);
     }
 
     $scope.Show = function() {
-		    	$scope.classname="forthevisibleform"	    
+		    	//$scope.classname="forthevisibleform"
+		    	if ($scope.counter==0)
+		    		$scope.showthis=true;
+		    	if ($scope.counter==1)
+		    		$scope.showthis1=true; 
+
+		    	$scope.counter++;   
 
     }
 
